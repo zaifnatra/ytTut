ea047bdb492f53a202aeb488cbb5b9f929d27cb8
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-06-18 19:25:29 -0400

    mongodb connecyion

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
-const express = require('express');
+dotenv.config();
+const app = express();
+
+app.get('/products', (req, res) => {
+   
+}
+);
+
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  connectDB();
+  console.log('Server is running on port http://localhost:3000');
 });
+

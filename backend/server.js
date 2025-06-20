3e5d25018fb6b80801e401dab5a4d389bd7c74b4
huzaifa <146299744+zaifnatra@users.noreply.github.com>
2025-06-20 11:11:18 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/Product.js';
 
 dotenv.config();
 const app = express();
 
-app.post('/products', (req, res) => {
-   
-}
-);
+app.use(express.json()); // to parse JSON bodies
 
+app.post('/products', async (req, res) => {
+  const product = req.body;
 
-app.listen(3000, () => {
-  connectDB();
-  console.log('Server is running on port http://localhost:3000');
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ message: 'Name, price, and image are required' });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, product: newProduct });
+  } catch (error) {
+    console.error("Error saving product:", error.message);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
+
+
+
+
+connectDB(); // connect to DB first
+app.listen(3000, () => {
+  console.log('Server is running on port http://localhost:3000');
+});
